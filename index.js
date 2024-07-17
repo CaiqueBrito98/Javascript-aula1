2ec2efcf2b25be9a2b277df2e26147117c15623f
Caique Brito <britocaique2016@gmail.comm>
2024-07-16 21:40:00 -0300

    atividade aula 1 alert

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// <---------------INICIO DAS VARIAVEIS---------------------->//
+
+var nome = "Caique";
+var sobrenome = "Brito";
+var idade = 26;
+var ano = 2024;
+
+//<----------------FIM DAS VARIAVEIS------------------>//
+
+//<-------------CONCATENAÇÃO----------------------->//
+
+var  resultado1 = nome + " " +sobrenome ;
+var  resultado2 = idade + " " + ano ; 
+var  resultado3 = ano - idade ; 
+
+//<-------------FIM DA CONCATENAÇÃO--------------->//
+
+//<-------------- FUNÇÕES--------------->//
+var numero1 = parseInt ( prompt ('digite um número')) 
+var numero2 = parseInt ( prompt ('digite outro número')) 
+var resultado  = numero1 + numero2   
+alert (`A soma entre ${numero1} e ${numero2} é igual ${resultado}`) 
+
+
+
